f006a609ed3cd85f3fb4d9d058247b91293b88f6
wichadak <duangdao@gmail.com>
2024-04-21 16:29:21 +0700

    fix app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -6,7 +6,7 @@ test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
 });
-test('getDay returns the month of the year', () => {
+test('getMonth returns the month of the year', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
-    expect( month ).toBe('November');
+    expect( month ).toBe('March');
 });
